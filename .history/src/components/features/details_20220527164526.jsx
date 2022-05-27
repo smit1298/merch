2ec903a747286f6919717ab6c434541a12bd9d5a
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-05-27 22:19:36 +0100

    features
    working on features mobile responsiveness

---
+++ .history/src/components/features/details_20220527164526.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./features.css";
+
+function Details(props) {
+  const features = props.features;
+  const listFeatures = features
+    ? features.map((feature) => {
+        return (
+          <li key={feature}>
+            <img src="./Assets/icons/check.png" alt={props.name} /> &nbsp;
+            {feature}
+          </li>
+        );
+      })
+    : null;
+
+  return (
+    <>
+      <div className="top">
+        <h4>{props.top}</h4>
+        <h3>{props.head}</h3>
+        <p>{props.brief}</p>
+      </div>
+
+      <div className="feature-cont">
+        <h6 >{props.Icon}</h6>
+        <img src={props.Image} alt={props.name} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        <ul>{listFeatures}</ul>
+      </div>
+    </>
+  );
+}
+
+export default Details;
---
+++ .history/src/components/features/details_20220527172401.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./features.css";
+
+function Details(props) {
+  const features = props.features;
+  const listFeatures = features
+    ? features.map((feature) => {
+        return (
+          <li key={feature}>
+            <img src="./Assets/icons/check.png" alt={props.name} /> &nbsp;
+            {feature}
+          </li>
+        );
+      })
+    : null;
+
+  return (
+    <section features>
+      <div className="top">
+        <h4>{props.top}</h4>
+        <h3>{props.head}</h3>
+        <p>{props.brief}</p>
+      </div>
+
+      <div className="feature-cont">
+        <img src={props.Image} alt={props.name} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        <ul>{listFeatures}</ul>
+      </div>
+    </section>
+  );
+}
+
+export default Details;
