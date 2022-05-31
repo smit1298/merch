c4acfefde26922486ddc5ff4bc4e6834875d64be
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-05-31 12:47:23 +0100

    responsive footer
    footer completed

---
+++ .history/src/components/footer/Footer_20220530191450.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <Container fluid className="footer">
+      <Row className="text-center">
+        <Col className="footer-img">
+          <img src="./Assets/images/logo.jpg" alt="merch.xyz" />
+        </Col>
+      </Row>
+      <Row className=" text-center">
+        <div className="footlink d-flex justify-content-center  text-center">
+          <a href="/">Overview</a>
+          <a href="/">Features</a>
+          <a href="/">Help</a>
+          <a href="/">Policy</a>
+        </div>
+      </Row>
+      <Row>
+        <div className="foot-small">
+          <small>&copy; 2022 Merch.xyz. All rights reserved.</small>
+        <a href="http://">terms</a>
+        </div>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
